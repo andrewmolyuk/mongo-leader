7533428374d2595d858befbf6dd8c6f33bd4c076
Andrew Molyuk <andrew.molyuk@gmail.com>
2017-11-18 16:31:54 +0200

    Assert added. Eslint errors fixed.

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
-const { Leader} = require('./index');
+const { Leader } = require('./index');
 const { MongoClient } = require('mongodb');
+const assert = require('assert');
 
 const url = 'mongodb://localhost:27017/test';
 
 MongoClient.connect(url, function (err, db) {
+  assert.equal(null, err);
+
   const leader = new Leader(db);
   setInterval(() => {
-    leader.isLeader().then(leader => console.log(`Am I leader? : ${leader}`))
+    leader.isLeader().then(leader => console.log(`Am I leader? : ${leader}`));
   }, 100);
 });
