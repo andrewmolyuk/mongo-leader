eb4614e9561946c7a55fb0688409b242476649dc
Andrew Molyuk <andrew@molyuk.com>
2024-04-02 23:35:22 +0300

    Remove Prettier and ESLint configuration files, update dependencies, and refactor code

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const crypto = require('crypto');
-const { EventEmitter } = require('events');
+const crypto = require('crypto')
+const { EventEmitter } = require('events')
 
 class Leader extends EventEmitter {
   constructor(db, options) {
-    super();
-    options = options || {};
-    this.id = crypto.randomBytes(32).toString('hex');
-    this.db = db;
-    this.options = {};
-    this.options.ttl = Math.max(options.ttl || 0, 1000); // Lock time to live
-    this.options.wait = Math.max(options.wait || 0, 100); // Time between tries to be elected
+    super()
+    options = options || {}
+    this.id = crypto.randomBytes(32).toString('hex')
+    this.db = db
+    this.options = {}
+    this.options.ttl = Math.max(options.ttl || 0, 1000) // Lock time to live
+    this.options.wait = Math.max(options.wait || 0, 100) // Time between tries to be elected
     this.key =
       'leader-' +
       crypto
         .createHash('sha1')
         .update(options.key || 'default')
-        .digest('hex');
+        .digest('hex')
 
-    this.initDatabase().then(() => this.elect());
+    this.initDatabase().then(() => this.elect())
   }
 
   initDatabase() {
@@ -27,20 +27,20 @@ class Leader extends EventEmitter {
         this.db.admin().command({ setParameter: 1, ttlMonitorSleepSecs: 1 })
       )
       .then(() => this.db.listCollections({ name: this.key }))
-      .catch(err => {
-        if (err.message !== 'ns not found') throw err;
+      .catch((err) => {
+        if (err.message !== 'ns not found') throw err
       })
-      .then(cursor =>
+      .then((cursor) =>
         cursor.hasNext()
           ? this.db.collection(this.key)
           : this.db.createCollection(this.key)
       )
-      .then(collection =>
+      .then((collection) =>
         collection.createIndex(
           { createdAt: 1 },
           { expireAfterSeconds: this.options.ttl / 1000, background: true }
         )
-      );
+      )
   }
 
   isLeader() {
@@ -48,11 +48,11 @@ class Leader extends EventEmitter {
       .collection(this.key)
       .findOne({ 'leader-id': this.id })
       .then(
-        item =>
+        (item) =>
           item !== null &&
           typeof item !== 'undefined' &&
           item['leader-id'] === this.id
-      );
+      )
   }
 
   elect() {
@@ -63,14 +63,14 @@ class Leader extends EventEmitter {
         { $setOnInsert: { 'leader-id': this.id, createdAt: new Date() } },
         { upsert: true, new: false }
       )
-      .then(result => {
+      .then((result) => {
         if (result?.lastErrorObject?.updatedExisting) {
-          setTimeout(() => this.elect(), this.options.wait);
+          setTimeout(() => this.elect(), this.options.wait)
         } else {
-          this.emit('elected');
-          setTimeout(() => this.renew(), this.options.ttl / 2);
+          this.emit('elected')
+          setTimeout(() => this.renew(), this.options.ttl / 2)
         }
-      });
+      })
   }
 
   renew() {
@@ -81,15 +81,15 @@ class Leader extends EventEmitter {
         { $set: { 'leader-id': this.id, createdAt: new Date() } },
         { upsert: false, new: false }
       )
-      .then(result => {
+      .then((result) => {
         if (result?.lastErrorObject?.updatedExisting) {
-          setTimeout(() => this.renew(), this.options.ttl / 2);
+          setTimeout(() => this.renew(), this.options.ttl / 2)
         } else {
-          this.emit('revoked');
-          setTimeout(() => this.elect(), this.options.wait);
+          this.emit('revoked')
+          setTimeout(() => this.elect(), this.options.wait)
         }
-      });
+      })
   }
 }
 
-module.exports = { Leader };
+module.exports = { Leader }
