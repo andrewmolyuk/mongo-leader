55c286be94c52484f19456615a540e537b96b8e0
Andrew Molyuk <andrew@molyuk.com>
2024-04-05 13:56:39 +0300

    Add example case

---
+++ example/test.js
@@ -0,0 +1,11 @@
+const { Leader } = require('mongo-leader')
+const { MongoClient } = require('mongodb')
+
+const url = 'mongodb://localhost:27017'
+
+MongoClient.connect(url).then((client) => {
+  const leader = new Leader(client.db('test'))
+  setInterval(() => {
+    leader.isLeader().then((leader) => console.log(`Am I leader? : ${leader}`))
+  }, 100)
+})
